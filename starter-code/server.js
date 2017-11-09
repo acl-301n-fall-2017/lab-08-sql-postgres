88a77b43199c8d6b51d4a662a45267d60d3a1087
Heather Knight <heather.knight.pdx@gmail.com>
2017-11-08 20:43:08 -0800

    completed the remaining TODOs

--- starter-code/server.js
+++ starter-code/server.js
@@ -13,8 +13,8 @@ const app = express();
 // TODO: Complete the connection string for the url that will connect to your local postgres database
 // Windows and Linux users; You should have retained the user/pw from the pre-work for this course.
 // Your url may require that it's composed of additional information including user and password
-const conString = 'postgres://USER:PASSWORD@HOST:PORT/DBNAME';
-const conString = 'postgres://localhost:5432';
+const conString = 'postgres://heather_knight@localhost:5432/lab08';
+// const conString = 'postgres://localhost:5432';
 
 // TODO: Our pg module has a Client constructor that accepts one argument: the conString we just defined.
 //       This is how it knows the URL and, for Windows and Linux users, our username and password for our
